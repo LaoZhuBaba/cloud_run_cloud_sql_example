540f5c5f3d050c922c552791f57d452f3207682c
David Liebert <david.liebert@kasna.com.au>
2025-05-17 19:34:32 +1200

    Tidied go code

--- app/main.go
+++ app/main.go
@@ -23,58 +23,6 @@ type visit struct {
 	VisitTime time.Time
 }
 
-// func connectWithConnector() (*sql.DB, error) {
-// 	mustGetenv := func(k string) string {
-// 		v := os.Getenv(k)
-// 		if v == "" {
-// 			log.Fatalf("Fatal Error in connect_connector.go: %s environment variable not set.\n", k)
-// 		}
-// 		return v
-// 	}
-// 	// Note: Saving credentials in environment variables is convenient, but not
-// 	// secure - consider a more secure solution such as
-// 	// Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
-// 	// keep passwords and other secrets safe.
-// 	var (
-// 		dbUser                 = mustGetenv("DB_USER")                  // e.g. 'my-db-user'
-// 		dbPwd                  = mustGetenv("DB_PASS")                  // e.g. 'my-db-password'
-// 		dbName                 = mustGetenv("DB_NAME")                  // e.g. 'my-database'
-// 		instanceConnectionName = mustGetenv("INSTANCE_CONNECTION_NAME") // e.g. 'project:region:instance'
-// 	)
-
-// 	dsn := fmt.Sprintf("user=%s password=%s database=%s", dbUser, dbPwd, dbName)
-// 	config, err := pgx.ParseConfig(dsn)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	var opts []cloudsqlconn.Option
-
-// 	// WithLazyRefresh() Option is used to perform refresh
-// 	// when needed, rather than on a scheduled interval.
-// 	// This is recommended for serverless environments to
-// 	// avoid background refreshes from throttling CPU.
-// 	opts = append(opts, cloudsqlconn.WithLazyRefresh())
-// 	d, err := cloudsqlconn.NewDialer(context.Background(), opts...)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	// Use the Cloud SQL connector to handle connecting to the instance.
-// 	// This approach does *NOT* require the Cloud SQL proxy.
-// 	config.DialFunc = func(ctx context.Context, network, instance string) (net.Conn, error) {
-// 		conn, err := d.Dial(ctx, instanceConnectionName, cloudsqlconn.WithPSC())
-// 		if err != nil {
-// 			log.Fatalf("Dial failed with error: %v", err)
-// 		}
-// 		return conn, nil
-// 	}
-// 	dbURI := stdlib.RegisterConnConfig(config)
-// 	dbPool, err := sql.Open("pgx", dbURI)
-// 	if err != nil {
-// 		return nil, fmt.Errorf("sql.Open: %w", err)
-// 	}
-// 	return dbPool, nil
-// }
-
 func connect(ctx context.Context) (*pgx.Conn, error) {
 	mustGetenv := func(k string) string {
 		v := os.Getenv(k)
@@ -83,21 +31,15 @@ func connect(ctx context.Context) (*pgx.Conn, error) {
 		}
 		return v
 	}
-	// Note: Saving credentials in environment variables is convenient, but not
-	// secure - consider a more secure solution such as
-	// Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
-	// keep passwords and other secrets safe.
 	var (
-		dbUser   = mustGetenv("DB_USER") // e.g. 'my-db-user'
-		dbPwd    = mustGetenv("DB_PASS") // e.g. 'my-db-password'
-		dbName   = mustGetenv("DB_NAME") // e.g. 'my-database'
+		dbUser   = mustGetenv("DB_USER")
+		dbPwd    = mustGetenv("DB_PASS")
+		dbName   = mustGetenv("DB_NAME")
 		dbIPAddr = mustGetenv("DB_IP_ADDRESS")
 		dbIPPort = mustGetenv("DB_IP_PORT")
 	)
 
 	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", dbUser, dbPwd, dbIPAddr, dbIPPort, dbName)
-
-	log.Printf("connection string: %s", fmt.Sprintf("postgres://%s:%s@%s:%s/%s", dbUser, "PW_NOT_SHOWN", dbIPAddr, dbIPPort, dbName))
 
 	conn, err := pgx.Connect(context.Background(), connStr)
 	if err != nil {
